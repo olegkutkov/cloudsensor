5322e64a427b0caf1f5c0f0e323a622ea087dd3a
Oleg Kutkov <elenbert@gmail.com>
2018-04-09 12:45:41 +0300

    db scripts for mlx and htu sensors

---
+++ scripts/ambient_sensor_to_db.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+put_to_db()
+{
+	temp=`echo $1 | awk '{print $1}' | awk -F '=' '{print $2}'`
+	humidity=`echo $1 | awk '{print $2}' | awk -F '=' '{print $2}'`
+
+	if [ -z "$temp" ]; then
+		echo "Failed to get temperature!"
+		exit 1
+	fi
+
+	if [ -z "$humidity" ]; then
+		echo "Failed to get humidity!"
+		exit 1
+	fi
+
+	sqlite3 /media/STORAGE/db/sensordata.db "insert into ambientsensor(temp, humid) values (\"${temp}\", \"${humid}\");"	
+}
+
+external_sensor=`/opt/allsky/bin/read_htu21d`
+
+put_to_db "${external_sensor}"
+
---
+++ scripts/mlx_sensor_to_db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+put_to_db()
+{
+	sensor_data=$1
+
+	skytemp=`echo $sensor_data | awk '{print $3}'`
+
+	sqlite3 /media/STORAGE/db/sensordata.db "insert into cloudsensor(temp) values (\"${skytemp}\");"
+}
+
+sensor_data=`/opt/allsky/bin/read_mlx90614 --bus 1 --i2c_addr 0x2a -i`
+
+put_to_db "${sensor_data}"
+
