7c27e082dd90420c125c67a4832c534baedda75d
Oleg Kutkov <kuvkov@yandex.ru>
2018-04-09 18:12:49 +0300

    device addr correction

--- scripts/mlx_sensor_to_db.sh
+++ scripts/mlx_sensor_to_db.sh
@@ -9,7 +9,7 @@ put_to_db()
 	sqlite3 /media/STORAGE/db/sensordata.db "insert into cloudsensor(temp) values (\"${skytemp}\");"
 }
 
-sensor_data=`/opt/cloudsens/bin/read_mlx90614 --bus 1 --i2c_addr 0x2a -i`
+sensor_data=`/opt/cloudsens/bin/read_mlx90614 --bus 1 --i2c_addr 0x5a -i`
 
 put_to_db "${sensor_data}"
 
