d52e5ed8a72469f06756184e56849c5dbf38c46d
Oleg Kutkov <kuvkov@yandex.ru>
2018-05-21 16:51:56 +0300

    correct init of the ram disk

--- scripts/init_ram_disk.sh
+++ scripts/init_ram_disk.sh
@@ -9,6 +9,10 @@ mkdir -p ${DST_WEB_DIR}
 mkdir -p ${DST_WEBDATA_DIR}
 mkdir -p ${DST_EPHEMDATA_DIR}
 
+chown allsky:allsky ${DST_WEB_DIR}/*
+chown allsky:allsky ${DST_WEBDATA_DIR}/*
+chown allsky:allsky ${DST_EPHEMDATA_DIR}*
+
 cp -v ${SRC_DIR}/*html ${DST_WEB_DIR}
 cp -v ${SRC_DIR}/*tpl ${DST_WEB_DIR}
 cp -v ${SRC_DIR}/*txt ${DST_WEB_DIR}
@@ -31,6 +35,9 @@ cp -fv ${SRC_DIR}/browserconfig.xml ${DST_WEB_DIR}
 /opt/cloudsens/bin/webdatagen/generate_web_data.sh sensors-week > /runtime_storage/webdata/webdatagen_week_last.log 2>&1
 /opt/cloudsens/bin/webdatagen/generate_web_data.sh sensors-month > /runtime_storage/webdata/webdatagen_month_last.log 2>&1
 /opt/cloudsens/bin/webdatagen/generate_web_data.sh sensors-year > /runtime_storage/webdata/webdatagen_year_last.log 2>&1
+
+/opt/cloudsens/bin/webdatagen/generate_web_data.sh system-report-sensors > /runtime_storage/webdata/webdatagen_system_sensors_last.log 2>&1
+/opt/cloudsens/bin/webdatagen/generate_web_data.sh system-report > /runtime_storage/webdata/webdatagen_system_all_last.log 2>&1
 
 chown allsky:allsky ${DST_WEB_DIR}/*
 chown allsky:allsky ${DST_WEBDATA_DIR}/*
